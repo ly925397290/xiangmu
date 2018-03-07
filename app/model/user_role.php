8b2e96d3ea51b58ceaefce9043ecbf95ea11a155
liyang <925397290@qq.com>
2018-03-07 17:36:35 +0800

    第五次

---
+++ app/model/user_role.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class user_role extends Model
+{
+    //链接用户和角色关系表
+    protected $table = 'user_role';
+    /**
+     * 该模型是否被自动维护时间戳
+     *
+     * @var bool
+     */
+    public $timestamps = false;
+
+    protected $guarded = [];
+
+    /**
+     * 链接用户与角色多对多关系
+     */
+    public function user_permission()
+    {
+    	return $this->belongsToMany('App\model\permission','role_permission','role_id','permission_id');
+    }
+}
